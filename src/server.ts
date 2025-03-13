c62dd6679342ff115c465e2845acb080f2518593
Aalok12345 <aalokghimire12@gmail.com>
2025-03-13 12:14:35 +0000

    install algokit

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import weatherRoute from "./routes/weatherRoutes.js";
 
 // We will create an express app
  const app = express();
@@ -6,6 +7,11 @@ import express from "express";
  // The port that the express server will listen on
  const PORT = 3000;
  app.use(express.json());
+
+ //our new route
+app.use("/api/weather", weatherRoute);
+
+
  // Start the express server
  app.listen(PORT, () => {
  console.log(`Server is listening on port ${PORT}`);
